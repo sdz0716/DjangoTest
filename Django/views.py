9017eed85b635776ec65cb1469fde6b7c80ba50f
daize.song <daize.song@fairlinkcentury.com>
2017-04-10 18:18:42 +0800

    update

--- Django/views.py
+++ Django/views.py
@@ -1,13 +1,31 @@
 from django.http import HttpResponse, Http404
 import datetime
+from django.template import Template, Context
+from django.template.loader import get_template
+from django.shortcuts import render_to_response
 
 def hello(request):
     return HttpResponse('hello world')
 
+#方法一
+# def current_datetime(request):
+#     now = datetime.datetime.now()
+#     html = '<html><body>It is now %s.</html></body>' % now
+#     return HttpResponse(html)
+
+#方法二
+# def current_datetime(request):
+#     now = datetime.datetime.now()
+#     # tem = Template('<html><body>It is now {{ var }}.</html></body>')
+#     tem = get_template('current_datetime.html')
+#     html = tem.render({'var': now})
+#     # html = tem.render(Context({'var': now}))  使用get_template方法时，render不接受Context(上下文)类型，只接受dict。如在试图里直接写html，则需要在render中使用Context()。
+#     return HttpResponse(html)
+
+#方法三
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = '<html><body>It is now %s' % now
-    return HttpResponse(html)
+    return render_to_response('current_datetime.html', {'var': now})    #render_to_response() 返回 HttpResponse 对象。第一个参数必须是要使用的模板名称。 如果要给定第二个参数，那么该参数必须是为该模板创建 Context 时所使用的字典。 如果不提供第二个参数， render_to_response() 使用一个空字典。
 
 def hours_ahead(request, offset):
     try:
