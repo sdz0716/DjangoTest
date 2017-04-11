8e9a5da2ce384f2371d38fb44ce7fe22637ca97e
daize.song <daize.song@fairlinkcentury.com>
2017-04-11 18:20:35 +0800

    update

--- Django/settings.py
+++ Django/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'Django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'root',
+        'PASSWORD': '111111',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- Django/views.py
+++ Django/views.py
@@ -33,5 +33,5 @@ def hours_ahead(request, offset):
     except:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = '<html><body>In %s hour(s), it will be %s.</body></html>' % (offset, dt)
-    return HttpResponse(html)
+    # html = '<html><body>In %s hour(s), it will be %s.</body></html>' % (offset, dt)
+    return render_to_response('hours_ahead.html', {'hour_offset': offset, 'next_time': dt})
