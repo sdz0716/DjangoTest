105ecc654f38fd775ddcd5923f0219dc8d9a2fab
daize.song <daize.song@fairlinkcentury.com>
2017-04-13 18:16:37 +0800

    update

--- Django/urls.py
+++ Django/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from Django.views import *
 
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url(r'^hello/$', hello),
     url(r'^time/$', current_datetime),
     url(r'^time/plus/(\d{1,2})/$', hours_ahead)
--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from books.models import Publisher, Book, Author
+
+admin.site.register(Publisher)
+admin.site.register(Book)
+admin.site.register(Author)
