23f8cdcea4669711b8c94510c104b4b5a6ab58bf
daize.song <daize.song@fairlinkcentury.com>
2017-04-20 18:27:29 +0800

    update

--- books/admin.py
+++ books/admin.py
@@ -4,6 +4,17 @@ from django.contrib import admin
 
 from books.models import Publisher, Book, Author
 
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email')
+    search_fields = ('first_name', 'last_name')
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'publisher', 'publication_date')
+    list_filter = ('publication_date', )
+    date_hierarchy = 'publication_date'
+    ordering = ('-publication_date', )
 admin.site.register(Publisher)
-admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
+# admin.site.register(Author)
+admin.site.register(Author, AuthorAdmin)
